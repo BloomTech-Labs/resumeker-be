afa94ad216e41b240e947392c2d639c3854d4383
Charlie <charles.j.montoya@gmail.com>
2020-04-27 20:03:41 -0400

    cleaned User typeDef

--- api/schema/typeDefs.js
+++ api/schema/typeDefs.js
@@ -1,42 +1,24 @@
 const { gql } = require("apollo-server-express");
 
 module.exports = gql`
-	type User {
-		id: ID!
-		email: String
-		firstName: String
-		lastName: String
-	}
-	type Token {
-		token: String
-	}
-	type hello {
-		message: String
-	}
-	type userInfo {
-		userInfo: String
-	}
-	type Query {
-		getUser: userInfo
-		allUsers: [User]!
-		getToken(token: String): Token
-		getUpdatedUser: userInfo
-		getString(bla: String): hello
-	}
-	input CreateUserInput {
-		id: ID!
-		email: String!
-		userImageURL: String!
-		firstName: String!
-		lastName: String!
-	}
-	type Mutation {
-		createUser(data: CreateUserInput!): User
-		getUpdatedUser(
-			id: ID
-			email: String
-			firstName: String
-			lastName: String
-		): userInfo
-	}
+    type Query {
+        getUser: User
+        allUsers: [User]!
+        getUpdatedUser: User
+    }
+    type Mutation {
+        createUser(data: CreateUserInput!): User
+    }
+    type User {
+        id: ID!
+        firstName: String
+        lastName: String
+        email: String
+    }
+    input CreateUserInput {
+        id: ID
+        firstName: String!
+        lastName: String
+        email: String
+    }
 `;
