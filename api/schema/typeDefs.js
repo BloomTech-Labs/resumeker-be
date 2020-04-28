12b00b53391dc123e5c94458234b779e862d250f
Charlie <charles.j.montoya@gmail.com>
2020-04-27 22:05:21 -0400

    Worked on Schema

--- api/schema/typeDefs.js
+++ api/schema/typeDefs.js
@@ -11,4 +11,68 @@ module.exports = gql`
         lastName: String
         email: String
     }
+    type WorkHistory {
+        id: ID!
+        startDate: DateTime
+        endDate: DateTime
+        title: String
+        description: String
+        company: String
+    }
+    type EducationHistory {
+        id: ID!
+        schoolType: SchoolType
+        schoolName: String
+        startDate: DateTime
+        endDate: DateTime
+        certName: String
+    }
+    enum SchoolType {
+        HIGH_SCHOOL_OR_EQUIVALENT
+        UNDERGRADUATE
+        BOOTCAMP_OR_EQUIVALENT
+        POST_SECONDARY
+    }
+    type Skills {
+        id: ID!
+        skillType: SkillType
+    }
+    enum SkillType {
+        TECHNICAL
+        QUALITATIVE
+    }
+    type Project {
+        id: ID!
+        title: String
+        role: Role
+        projectUrl: String
+        description: String
+    }
+
+    # Role is the Job the user is applying for
+    type Role {
+        id: ID!
+        name: String
+        description: String
+    }
+    type DisplayQuestion {
+        id: ID!
+        text: String
+        role: Role
+    }
+    type AnsweredQuestion {
+        id: ID!
+        questionText: String
+        answerText: String
+        user: User
+    }
+    type Hobby {
+        id: ID!
+        name: String
+    }
+    type Draft {
+        id: ID!
+
+        # To Be Continued...
+    }
 `;
