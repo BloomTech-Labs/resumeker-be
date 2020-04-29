ea153407584a477ea99ce11dc9810e3b25f35265
Barbara Mayfield <bmayfield1991@gmail.com>
2020-04-28 19:15:55 -0500

    Added scalar DataType to typeDefs.js, commented out mutations in resolver file

--- resolvers/resolvers.js
+++ resolvers/resolvers.js
@@ -2,44 +2,35 @@ const db = require("../database/config/dbConfig");
 const { getUser, updateUser } = require("../middleware/m2mRouter");
 
 module.exports = {
-	Query: {
-		getUser: async (parent, _, context) => {
-			const user = await getUser(context.token);
+    Query: {
+        getUser: async (parent, _, context) => {
+            const user = await getUser(context.token);
 
-			console.log(user, "getUser User");
+            console.log(user, "getUser User");
 
-			return { userInfo: JSON.stringify(user) };
-		},
-		// getUpdatedUser: async (parent, _, context) => {
+            return { userInfo: JSON.stringify(user) };
+        },
+        // getUpdatedUser: async (parent, _, context) => {
 
-		//         const updated_user = await updateUser(context.user_info);
+        //         const updated_user = await updateUser(context.user_info);
 
-		//         console.log(updated_user, "updated user");
+        //         console.log(updated_user, "updated user");
 
-		//         return {userInfo: JSON.stringify(updated_user)};
-		//     },
-		allUsers: (parent, args, _) => {
-			return db("users");
-		},
-		getToken: (parent, args, context) => {
-			return context;
-		},
-		getString: (_, { bla }) => {
-			return { message: `Hello ${bla}` };
-		},
-	},
-	Mutation: {
-		createUser: (parent, { data }, _) => {
-			return db("users").insert({ data });
-		},
-		getUpdatedUser: async (_, args, context) => {
-			console.log(args, "getUpdatedUser Body");
+        //         return {userInfo: JSON.stringify(updated_user)};
+        //     },
+        getAllUsers: (parent, args, _) => {
+            return db("users");
+        },
+    },
+    // Mutation: {
+    //     getUpdatedUser: async (_, args, context) => {
+    //         console.log(args, "getUpdatedUser Body");
 
-			const updatedUser = await updateUser(context.token, args);
+    //         const updatedUser = await updateUser(context.token, args);
 
-			console.log(updatedUser, "Updated User Holy Grail =====");
+    //         console.log(updatedUser, "Updated User Holy Grail =====");
 
-			return { userInfo: JSON.stringify(updatedUser) };
-		},
-	},
+    //         return { userInfo: JSON.stringify(updatedUser) };
+    //     },
+    // },
 };
