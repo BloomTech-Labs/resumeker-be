870d9be64164d9f7c5b34978cdbada24165c77c7
jacobcalv <jacobcalvino@outlook.com>
2020-05-27 20:30:04 -0500

    added test for adding education history

--- api/src/__tests__/education.test.js
+++ api/src/__tests__/education.test.js
@@ -1,8 +1,8 @@
 const { ApolloServer, gql, AuthenticationError } = require("apollo-server");
 const { createTestClient } = require("apollo-server-testing");
 
-const resolvers = require("../graphql/resolvers/index");
-const typeDefs = require("../graphql/schema/index");
+const resolvers = require("../graphql/resolvers/education");
+const typeDefs = require("../graphql/schema/education");
 
 const throwAuthError = () => {
     throw new AuthenticationError(
@@ -10,31 +10,69 @@ const throwAuthError = () => {
     );
 };
 
-it("testClient query", async () => {
+it("adding a new education and retrieving it", async () => {
     // create a test server to test against, using our production typeDefs,
     // resolvers, and dataSources.
     const server = new ApolloServer({
         typeDefs,
         resolvers,
         context: () => ({
-            decoded: { sub: "random string" },
+            decoded: { sub: process.env.TEST_USER },
             throwAuthError,
         }),
     });
 
     // use the test server to create a query function
-    const { query } = createTestClient(server);
+    const { query, mutate } = createTestClient(server);
 
     // run query against the server and snapshot the output
-    const res = await query({
-        query: gql`
-            query getHobby {
-                getHobby(hobbyID: 352) {
-                    name
+    const res_mutation = await mutate({
+        mutation: gql`
+            mutation addEducationTest {
+                addEducationHistory(input: {
+                    draftID: 1000,
+                    schoolType: COURSE,
+                    schoolName: "Lambda",
+                    startDate:"06.19.1990",
+                    endDate: "06.03.2020",
+                    certName: "A1 Degree",
+                    courses:"full stack web dev"
+                }) {
+                    id
+                    draftID
+                    schoolType
+                    schoolName
+                    startDate
+                    endDate
+                    certName
+                    courses
                 }
             }
         `,
-        variables: { hobbyID: 352 },
+       
     });
-    expect(res.data.getHobby).toBe(null);
+
+    const res_query = await query({
+        query: gql`
+            query getEducationByDraft ($draftID: ID!) {
+                getEducationByDraft(draftID: $draftID){
+                    id
+                    draftID
+                    schoolType
+                    schoolName
+                    startDate
+                    endDate
+                    certName
+                    courses
+                }
+            }
+        `,
+        variables: {draftID: 1000}
+    })
+    console.log(res_mutation)
+
+    const result = res_query.data.getEducationByDraft
+    const lastObject = result[result.length-1]
+
+    expect(res_mutation.data.addEducationHistory.id).toEqual(lastObject.id)
 });
